22a433a62a5e4121d3776fd0a742aa94d8cb2e26
Tyler Sellmayer <tyler.sellmayer@gmail.com>
2016-05-29 16:13:06 -0700

    SFM now with point cluster that looks maybe almost right.

--- main.m
+++ main.m
@@ -1,6 +1,5 @@
 %% Set up.
 clear all; close all; clc; rng default;
-addpath('sfm');
 
 % How many images to use when determining colors.
 COLOR_SUBSET_SIZE = 5;
@@ -10,6 +9,9 @@ TRACK_COLOR_SUBSET_SIZE = 20;
 
 % How many images to use when determining track width.
 TRACK_WIDTH_SUBSET_SIZE = 30;
+
+% Maximum distance from plane when fitting track planes.
+PLANE_FIT_THRESHOLD = 20;
 
 % Which images to use?
 image_folder = 'N_uV0Q2UH98';
@@ -35,28 +37,40 @@ subset_images = random_subset_images(image_paths, TRACK_WIDTH_SUBSET_SIZE);
 track_width_pixels = average_track_width(subset_images, track_color_centroid_idx, color_centroids)
 
 %% We're going to use track_width_pixels as scale factor, because we arbitrarily declare the real track width 1
-K = eye(3); K(1,1) = track_width_pixels; K(2,2) = track_width_pixels;
+K = eye(3); K(3,3) = track_width_pixels;
+cameraParams = cameraParameters('IntrinsicMatrix', K);
 
 %% Solve for [R t] matrices between frames.
 
 % TESTING ONLY: use a small set of frames.
-image_range = [3000 3010]
+image_range = [91 6119];
+image_range = [91 100]
 image_paths = cell(diff(image_range), 1);
 for i=image_range(1):image_range(2)
   image_paths{i - image_range(1) + 1} = sprintf('%s/%05d.png', image_folder, i);
 end
 
-% Initialize features in last image, which should be similar to the first
-% image because roller coasters are a loop.
-image2 = rgb2gray(imread(image_paths{end}));
+colorimage = imread(image_paths{1});
+image2 = rgb2gray(colorimage);
 points = detectSURFFeatures(image2);
 [features2,valid_points2] = extractFeatures(image2,points);
-track_points = zeros(numel(image_paths) + 1, 3);
-for i=1:numel(image_paths)
+
+% Collect track points.
+track_points = [];
+
+% Rotation and translation are cumulative, remember.
+cR = eye(3);
+cT = [0,0,0];
+
+for i=2:numel(image_paths)
+  % Figure out the image's colors.
+  colorimage = imread(image_paths{i});
+  [H W C] = size(colorimage);
+  pixel_list = reshape(colorimage, H*W, C);
+
   % Replace image1 with image2, then get a new image2.
   image1 = image2;
-  image2 = rgb2gray(imread(image_paths{i}));
-  [H W] = size(image2);
+  image2 = rgb2gray(colorimage);
   features1 = features2;
   valid_points1 = valid_points2;
 
@@ -70,13 +84,42 @@ for i=1:numel(image_paths)
   matchedPoints2 = valid_points2(index_pairs(:,2),:);
 
   % Estimate the fundamental matrix F.
-  F = estimateFundamentalMatrix(matchedPoints1,matchedPoints2,'Method','RANSAC','NumTrials',2000,'DistanceThreshold',1e-4);
-  E = K' * F * K;
+  [F, epipolarInliers] = estimateFundamentalMatrix(matchedPoints1,matchedPoints2,'Method','MSAC','NumTrials',2000);
 
   % Solve SFM.
-  Rt = computeRTFromE(E, [matchedPoints1.Location'; matchedPoints2.Location'], K, H, W);
+  inliers1 = matchedPoints1(epipolarInliers,:);
+  inliers2 = matchedPoints2(epipolarInliers,:);
+  [R, t] = cameraPose(F, cameraParams, inliers1, inliers2);
+  camMatrix1 = cameraMatrix(cameraParams, cR', -cT*cR');
+  cR = R * cR;
+  cT = cT * R' + t;
+  camMatrix2 = cameraMatrix(cameraParams, cR', -cT*cR');
 
-  track_points(i+1,:) = Rt * [track_points(i,:)'; 1];
+  % Compute the 3-D points.
+  points3D = triangulate(matchedPoints1, matchedPoints2, camMatrix1, camMatrix2);
+
+  % Compute the corresponding colors of each 3D points.
+  colorIdx = sub2ind([H,W], round(matchedPoints2.Location(:, 2)), round(matchedPoints2.Location(:, 1)));
+  pointColors = pixel_list(colorIdx, :);
+  
+  % Keep just the points which match the track color.
+  point_color_centroids = knnsearch(color_centroids, double(pointColors));
+  track_point_idxs = find(point_color_centroids == track_color_centroid_idx);
+  if isempty(track_points)
+    track_points = select(pointCloud(points3D), track_point_idxs);
+  else
+    track_points = pcmerge(track_points, select(pointCloud(points3D), track_point_idxs), 1);
+  end
 end
-figure();
-plot3(track_points(:,1), track_points(:,2), track_points(:,3), '.');
+
+%% Display Figure
+figure;
+axis tight auto;
+pcshow(track_points);
+v = VideoWriter('rotate_points.avi');
+open(v);
+for k = 1:180
+  view(k, 45);
+  writeVideo(v,getframe(gcf));
+end
+close(v);
